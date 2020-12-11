6e886ba7f8c728c2125785ff2baf5f48e1294ffd
Akash Takawale <atakawale207@gmail.com>
2020-12-11 19:41:24 +0530

    Made some corrections

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './collection.styles.scss'
 
 const CollectionPage = ({ match }) => {
+    console.log('This is collection component ')
     console.log(match);
+
     return (
         <div className="collection">
             <h2>CollectionPage</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima id quam facilis debitis laboriosam, consequuntur magnam consectetur sunt corporis quia quas doloremque maxime a aliquid ea rem quos, itaque eveniet!</p>
         </div>
     )
 }
