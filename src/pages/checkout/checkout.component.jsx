5ecd1a228fbc013e5ab209588c9e176718fc92fd
Akash Takawale <atakawale207@gmail.com>
2021-08-14 00:15:34 +0530

    made some corrections

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -38,7 +38,7 @@ const CheckoutPage = ({ total,cartItems }) => {
             <TestWarningContainer>
             *Please use the following test credit card for payments*
             <br />
-            4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+            4242 4242 4242 4242 - Exp: 01/25 - CVV: 123
             </TestWarningContainer>
             <StripeCheckoutButton price={total} />
         </CheckoutContainer>
